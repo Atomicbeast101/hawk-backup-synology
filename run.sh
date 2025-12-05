30ffc0d7b656af01dea554e428321e8bedd35a98
Atomicbeast101 <adam@brodack.net>
2025-12-05 17:00:01 -0600

    Changed SSH settings to allow username/password login

--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@
 echo "Performing backup..."
 PYTHON_SITE_PACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`
 export ANSIBLE_COLLECTIONS_PATH=$PYTHON_SITE_PACKAGES/ansible_collections
-export ANSIBLE_HOST_KEY_CHECKING=False
 .venv/bin/ansible-playbook playbook.yml -i $SYNOLOGY_HOST,
 echo "Backup completed!"
