d112b58bab541927541496e94900d3221a1d7585
Atomicbeast101 <adam@brodack.net>
2025-12-06 14:10:38 -0600

    Fixed ansible_user variable

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 echo "Performing backup..."
 PYTHON_SITE_PACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`
 export ANSIBLE_COLLECTIONS_PATH=$PYTHON_SITE_PACKAGES/ansible_collections
-.venv/bin/ansible-playbook playbook.yml -i $SYNOLOGY_HOST,
+.venv/bin/ansible-playbook playbook.yml -i $SYNOLOGY_HOST, -vvvv
 echo "Backup completed!"
