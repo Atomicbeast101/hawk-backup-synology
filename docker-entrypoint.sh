1317c231bf52ec8cf76841af37d9e68821b3bd32
Atomicbeast101 <adam@brodack.net>
2025-12-10 07:58:42 -0600

    Fixed working directory for run script & load env variables so cron can access them

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,10 @@ echo "Setting up environment..."
 
 # Configure cron schedule
 echo "Scheduling backup job..."
-echo "$CRON_SCHEDULE /app/run.sh >> /var/log/cron.log 2>&1" > /etc/crontabs/root
+echo "$CRON_SCHEDULE cd /app && /app/run.sh >> /var/log/cron.log 2>&1" > /etc/crontabs/root
+
+# Load all env variables to /etc/environment for cron to access
+printenv > /etc/environment
 
 # Run cron in foreground
 echo "Started cron job!"
